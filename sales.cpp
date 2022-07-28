38236a008a244448431c58b16fb53239a235d91f
YL0101 <coffee0101@kakao.com>
2022-07-28 20:23:54 +0900

    매출 그래프 추가

--- sales.cpp
+++ sales.cpp
@@ -1,24 +1,22 @@
 #include "sales.h"
 #include "ui_sales.h"
-#include <QList>
-#include <QtCharts/QChartView>
-#include <QtCharts/QLineSeries>
-#include <QtCharts/QChartGlobal>
-#include <QtCharts>
+
 
 sales::sales(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::sales)
 {
     ui->setupUi(this);
+    now = QDate::currentDate();
     dailysales();
 }
 
 void sales::dailysales()
 {
-    QDate now = QDate::currentDate();
+
     QList<int> list;
     QList<QString> days;
+    // 현재 날짜로부터 6일전
     QDate start_day = now.addDays(-6);
     for(int i = 0; i<7; i++)
     {
@@ -32,8 +30,8 @@ void sales::dailysales()
         list.append(date_sales);
         start_day = start_day.addDays(1);
     }
-
-    QLineSeries *series = new QLineSeries();
+    // line chart
+    series = new QLineSeries();
     int max = list[0];
     for(int i=0; i<7; i++)
     {
@@ -42,31 +40,77 @@ void sales::dailysales()
         {
             max = total;
         }
+        // line chart에 점 추가
         series->append(i, total);
     }
 
-    QChart *chart = new QChart();
+    chart = new QChart();
+    //범례 숨기기
     chart->legend()->hide();
     chart->addSeries(series);
     QStringList dates;
     dates << days[0] << days[1] << days[2] << days[3] << days[4] << days[5] << days[6];
-    QBarCategoryAxis *axisX = new QBarCategoryAxis();
+    // x축 눈금 설정
+    axisX = new QBarCategoryAxis();
     axisX->append(dates);
     chart->addAxis(axisX, Qt::AlignBottom);
+    // line chart에 x축 추가축
     series->attachAxis(axisX);
+    // x축 범위 설정
     axisX->setRange(days[0], days[6]);
-    QValueAxis *axisY = new QValueAxis();
+    // y축
+    axisY = new QValueAxis();
     chart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisY);
     axisY->setRange(0, max);
     // QChartView에 chart입력
-    QChartView *chartView = new QChartView(chart);
+    chartView = new QChartView(chart);
     // chartView속성 설정
     chartView->setRenderHint(QPainter::Antialiasing);
+    // layout에 chartview 표시
     ui->verticalLayout->addWidget(chartView);
+
+    ui->tableWidget->clear();
+    ui->tableWidget->setRowCount(7);
+    ui->tableWidget->setColumnCount(2);
+    ui->tableWidget->setHorizontalHeaderItem(0, new QTableWidgetItem("날짜"));
+    ui->tableWidget->setHorizontalHeaderItem(1, new QTableWidgetItem("매출"));
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    ui->tableWidget->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    for(int i=0; i<7; i++)
+    {
+        ui->tableWidget->setItem(i, 0, new QTableWidgetItem(days[i]));
+        ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString::number(list[i])));
+    }
+
 }
+
 
 sales::~sales()
 {
     delete ui;
 }
+
+void sales::on_before_btn_clicked()
+{
+    // 7일전
+    now = now.addDays(-7);
+    ui->verticalLayout->removeWidget(chartView);
+    dailysales();
+}
+
+void sales::on_next_btn_clicked()
+{
+    now = now.addDays(7);
+    QDate current = QDate::currentDate();
+    int diff = current.daysTo(now);
+    if(diff>0)
+    {
+        now = QDate::currentDate();
+        QMessageBox::information(this, "", "최근");
+    }
+    ui->verticalLayout->removeWidget(chartView);
+    dailysales();
+}
+
--- sales.h
+++ sales.h
@@ -2,6 +2,12 @@
 #define SALES_H
 #include "database.h"
 #include <QDialog>
+#include <QList>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QChartGlobal>
+#include <QtCharts>
+#include <QCloseEvent>
 
 namespace Ui {
 class sales;
@@ -16,11 +22,23 @@ public:
     ~sales();
     void dailysales();
 
+
+private slots:
+    void on_before_btn_clicked();
+
+    void on_next_btn_clicked();
+
 private:
     Ui::sales *ui;
     std::string query;
     QSqlQuery sql;
     QSqlRecord record;
+    QChartView *chartView;
+    QLineSeries *series;
+    QDate now;
+    QChart *chart;
+    QBarCategoryAxis *axisX;
+    QValueAxis *axisY;
 
 };
 
